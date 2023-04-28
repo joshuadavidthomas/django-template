bbd5fb249ab9397fa61e15da25596cd2199aa212
Josh <josh@joshthomas.dev>
2023-04-27 21:25:26 -0500

    add missing views

--- project/core/views.py
+++ project/core/views.py
@@ -3,6 +3,19 @@ from __future__ import annotations
 from django.shortcuts import render
 from django.utils import timezone
 from django.views.decorators.http import require_GET
+from sentry_sdk import last_event_id
+
+
+def custom_error_404(request, exception=None, *args, **kwargs):
+    response = render(request, "404.html", context={}, status=404)
+    return response
+
+
+def custom_error_500(request, *args, **kwargs):
+    response = render(
+        request, "500.html", context={"sentry_event_id": last_event_id()}, status=500
+    )
+    return response
 
 
 @require_GET
