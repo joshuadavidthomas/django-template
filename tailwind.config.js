36168153481b39519069fd9d6aad3ef5eb68b390
Josh <josh@joshthomas.dev>
2023-05-23 16:19:41 -0500

    add default colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultColors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./static/**/*.{js,ts,jsx,tsx}", "./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: defaultColors.indigo,
+        secondary: defaultColors.blue,
+        tertiary: defaultColors.green,
+        aspect: defaultColors.orange,
+      },
+    },
   },
   plugins: [],
 };
